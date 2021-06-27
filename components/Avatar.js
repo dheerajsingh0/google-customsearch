d652876a35f98e74861b04e0aea2d8295846f297
Dheeraj singh <47826524+Dheerajsingh0@users.noreply.github.com>
2021-06-27 22:43:34 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,7 +2,7 @@ const Avatar = ({url,className}) => {
     return (
         <img  loading="lazy" 
         className={`h-8  rounded-full  cursor-pointer transition duration-150 transform hover:scale-110 animate-bounce  hover:animate-none ${className}`}
-         src={url} alt="profile picture"/>
+         src="dp.jpg" alt="profile picture"/>
     )
 }
 
